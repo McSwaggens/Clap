02c3a889a553596801dfb3e61823c74b3768ee5f
McSwaggens <McSwaggens@protonmail.com>
2016-08-14 14:28:33 +1000

    Added status bar

---
+++ Clap/UI/StatusBar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Clap
+{
+	public class StatusBar : GraphicalInterface
+	{
+		public StatusBar (Clap clap) : base (clap)
+		{
+			this.clap = clap;
+			this.name = "Status Bar";
+		}
+		
+		public override void Update ()
+		{
+			GUI.FillRectangle (new Location (0, 0), new Location (size.Width, 0), ConsoleColor.Black);
+			string title = clap.currentSong == null ? "-No song playing-" : clap.currentSong.name;
+			GUI.DrawString (title, new Location (size.Width / 2, 0), ConsoleColor.Red, ConsoleColor.Black);
+		}
+    }
+}
+
